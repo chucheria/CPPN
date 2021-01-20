3a75593f8e13b68d5ff0d87c0909299fe4032764
chucheria <tatipatati@gmail.com>
2021-01-20 19:59:21 +0100

    add latent space

--- CPPN/generator.py
+++ CPPN/generator.py
@@ -14,6 +14,22 @@ class Generator:
         self.patterns['sin'] = self._generate_sin_input
         self.patterns['square'] = self._generate_square_input
         self.patterns['circular'] = self._generate_circular_input
+        self.patterns['latent'] = self._generate_latent_space
+
+    def _generate_latent_space(self):
+        x = np.arange(0, self.size_x, 1)
+        y = np.arange(0, self.size_y, 1)
+        colors = np.zeros((self.size_x, self.size_y, 4))
+        for i in x:
+            for j in y:
+                colors[i][j] = np.array([
+                    float(i) / self.size_y - self.scale,
+                    float(j) / self.size_y - self.scale,
+                    np.sqrt(((i * self.scale - (self.size_x * self.scale / 2)) ** 2) +
+                            ((j * self.scale - (self.size_y * self.scale / 2)) ** 2)),
+                    2])
+
+        return colors.reshape(self.size_x * self.size_y, 4)
 
     def _generate_circular_input(self):
         x = np.arange(0, self.size_x, 1)
@@ -23,7 +39,7 @@ class Generator:
             for j in y:
                 colors[i][j] = np.array([
                     float(i) / self.size_y - self.scale,
-                    float(j) / self.size_y - self.scale,
+                    float(j) / self.size_x - self.scale,
                     np.sqrt(((i * self.scale - (self.size_x * self.scale / 2)) ** 2) +
                             ((j * self.scale - (self.size_y * self.scale / 2)) ** 2))])
 
@@ -45,7 +61,8 @@ class Generator:
         colors = np.zeros((self.size_x, self.size_y, 2))
         for i in x:
             for j in y:
-                colors[i][j] = np.array([np.sin(i * self.scale) * self.size_x, j])
+                colors[i][j] = np.array([np.sin(i * self.scale) * self.size_x,
+                                         np.sin(j * self.scale) * self.size_y])
         return colors.reshape(self.size_x * self.size_y, 2)
 
     def _generate_input(self):
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from network import NN, plot_callback, plot_image
 from generator import Generator
+import torch.nn as nn
 
 
-OUTPUT_SIZE = 3
-SIZE_X, SIZE_Y = 256, 256
+OUTPUT_SIZE = 4
+SIZE_X, SIZE_Y = 1080, 1080
 SCALE = 0.1
-NEURONS = 8
-LAYERS = 16
+NEURONS = 16
+LAYERS = 9
+ACTIVATION = nn.Tanh
 
-net = NN(n_neurons=NEURONS, n_layers=LAYERS, output_size=OUTPUT_SIZE)
+net = NN(n_neurons=NEURONS, n_layers=LAYERS, output_size=OUTPUT_SIZE, activation=ACTIVATION, input_size=3)
 generator = Generator(size_x=SIZE_X, size_y=SIZE_Y, scale=SCALE)
-gen = generator.patterns['square']()
+gen = generator.patterns['circular']()
+#net.train(gen)
 
 plot_image(plot_callback(net, gen, size_x=SIZE_X, size_y=SIZE_Y, output_size=OUTPUT_SIZE))
-
-for n in range(0, 11):
-    net.train(gen, n_steps=n)
-    plot_image(plot_callback(net, gen, size_x=SIZE_X, size_y=SIZE_Y, output_size=OUTPUT_SIZE))
+#
+# for n in range(0, 11):
+#     print(n)
+#     print('---------')
+#     net.train(gen, n_steps=n)
+#     plot_image(plot_callback(net, gen, size_x=SIZE_X, size_y=SIZE_Y, output_size=OUTPUT_SIZE))
+#
