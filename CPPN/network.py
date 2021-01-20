36e800ccf714edefeb2533e0dcb5b8c98bb20f22
Ángel Delgado <angel.sherpa80@gmail.com>
2021-01-20 18:56:04 +0100

    feat: add train() method in NN class

--- CPPN/network.py
+++ CPPN/network.py
@@ -5,25 +5,20 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 
 
-def plot_image(colors, fig_size=6):
-    plt.figure(figsize=(fig_size, fig_size))
-    plt.imshow(colors, interpolation='nearest', vmin=0, vmax=1)
-    plt.show()
-
-
-def plot_callback(net, input, size_x, size_y, output_size=3):
-    img = net(torch.tensor(input).type(torch.FloatTensor)).detach().numpy()
-    return img.reshape(size_x, size_y, output_size)
-
-
 def save(colors, name):
     plt.imsave(f"images/{name}.png", colors)
 
 
+def plot_image(colors, size_x,size_y):
+    img = colors.detach().numpy()
+    img = img.reshape(size_x, size_y,3)*255
+    plt.figure(figsize=(6,6))
+    plt.imshow(img.astype(np.uint8))
+
 class NN(nn.Module):
 
     @staticmethod
-    def _init(m):
+    def init(m):
         if type(m) == nn.Linear:
             nn.init.normal_(m.weight)
     
@@ -35,38 +30,47 @@ class NN(nn.Module):
         layers += [activation()]
 
         for _ in range(n_layers - 1):
-            layers += [nn.Linear(n_neurons, n_neurons, bias=False)]
+            layers += [nn.Linear(n_neurons, n_neurons, bias=True)]
             layers += [activation()]
 
-        layers += [nn.Linear(n_neurons, output_size, bias=False), nn.Sigmoid()]
+        layers += [nn.Linear(n_neurons, output_size, bias=True), nn.Sigmoid()]
         self.layers = nn.Sequential(*layers)
-        self.apply(self._init)
+        self.apply(self.init)
         
     @property
     def optimizer(self):
         _opt = torch.optim.SGD(self.layers.parameters(), lr=0.1, momentum=0.9)
-        _opt.zero_grad()
+        _opt.zero_grad()       
         return _opt
-    
+
+    @property
+    def criterion(self):
+      return nn.MSELoss()
+
     @property
     def loss(self):
-        def _loss(img):
-            return torch.mean(1-torch.amin(img, dim=(0, 1))/torch.amax(img, dim=(0, 1)))
+        def _loss(result, target):
+            error = self.criterion(result, target)
+            print("loss: ",error.data)
+
+            return error*0.01
         return _loss
     
     def forward(self, x):
         return self.layers(x)
 
-    def train(self, x, n_steps=10, callback=lambda x: None):
-        self.apply(self._init)
+    def train(self, source, target, n_steps=10, callback=None):
+
         opt = self.optimizer
+        target = torch.tensor(target)
 
         for _ in range(n_steps):
 
-            img = self.layers(torch.tensor(x).type(torch.FloatTensor))
-            loss = self.loss(img)
+            result = self.forward(torch.tensor(source).type(torch.FloatTensor))
 
-            callback(img)
+            _ = callback(result) if callable(callback) else None
 
+            loss = self.loss(result,target)
             loss.backward()
             opt.step()
+            
